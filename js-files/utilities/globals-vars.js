1853a8ea9e52d89343fac70d117281122c6ed8c3
doublem <136604978+MarioMinchevski@users.noreply.github.com>
2024-01-19 10:16:52 +0100

    Add files via upload

---
+++ js-files/utilities/globals-vars.js
@@ -0,0 +1,19 @@
+// navbar vars 
+
+export const headerNav = document.querySelector('#header')
+export const landingPageNavText = document.querySelector('.landing-page-navtext')
+export const navInnerWrap = document.querySelector('.nav-inner-wrap')
+export const navVisitorPageNavText = document.querySelector('.visitor-page-navtext')
+export const navAuctionText = document.querySelector('.auction-page-text')
+export const navCurrentArtistName = document.querySelector('.current-artist-name')
+export const navMenu = document.querySelector('.nav-menu')
+export const hamburgerIcon = document.querySelector('.hamburger-menu')
+export const auctionIcon = document.querySelector('.auction-icon')
+
+export const navElements = [header, landingPageNavText, navInnerWrap, navVisitorPageNavText, navCurrentArtistName, navMenu, hamburgerIcon, auctionIcon, navAuctionText]
+
+// others
+
+export const popUpOverlay = document.querySelector('.pop-up-overlay')
+
+export const ANIMATION_DURATION = 250
